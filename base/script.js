6a164ee22a663e096df711c6f40f0a5caddb2dc0
lfpnk0 <4between5@gmail.com>
2015-11-15 20:43:39 -0700

    Update script.js

--- base/script.js
+++ base/script.js
@@ -1,15 +1,15 @@
 m.base = new Object();
-//m.base.pos = function(dir){
-//	if(!dir){dir = document.getElementById('base_location').value;}
-//	var w = document.getElementById('base_width').value; 
-//	var h = document.getElementById('base_height').value; 
-//	if(dir=='right'){ 
-//		window.moveTo(screen.availWidth-w,0); 
-//		window.resizeTo(w,h); 
-//	} 
-//	else{ 
-//		window.moveTo(0,0); 
-//		window.resizeTo(w,h); 
-//	} 
-//}
-//m.init.push(m.base.pos);
+m.base.pos = function(dir){
+	if(!dir){dir = document.getElementById('base_location').value;}
+	var w = document.getElementById('base_width').value; 
+	var h = document.getElementById('base_height').value; 
+	if(dir=='right'){ 
+		window.moveTo(screen.availWidth-w,0); 
+		window.resizeTo(w,h); 
+	} 
+	else{ 
+		window.moveTo(0,0); 
+		window.resizeTo(w,h); 
+	} 
+}
+m.init.push(m.base.pos);
