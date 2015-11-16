80f04050abd54d90426157a40b4615c16e048eb8
lfpnk0 <4between5@gmail.com>
2015-11-15 22:43:13 -0700

    Update m.js

--- m.js
+++ m.js
@@ -1,6 +1,7 @@
 (function(){
   var el = document.createElement('div');
   el.id='mainWrapper';
+  el.innerHTML = 'test';
   document.body.appendChild(el);
   el = document.createElement('div');
   el.id='console';
