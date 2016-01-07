446d93527362a3ac2990e3d84d6c12f835c8da1c
lfpnk0 <4between5@gmail.com>
2016-01-06 20:41:59 -0700

    Update mod.js

--- mod.js
+++ mod.js
@@ -1,4 +1,4 @@
-mod = new Object();
+mod = {};
 mod.options = [' none','menu_01','quickkeys_01','survey_01','messages_01','clock_01','links_01'];
 mod.options.sort();
 mod.defaults = ['menu_01','survey_01','survey_01','messages_01','clock_01','links_01'];
@@ -43,7 +43,7 @@ mod.listAdd = function(opt){
   document.getElementById('modPicker').appendChild(s);
   document.getElementById('modPicker').appendChild(document.createElement('br'));
   mod.winResize();
-}
+};
 mod.get = function(){
   var e = document.getElementsByClassName('module');
   for (var i = 0; i < e.length; i++) {
@@ -51,21 +51,21 @@ mod.get = function(){
   }
   var d = document.getElementById('modPicker');
   var m = d.getElementsByTagName('select');
-  for (var i = 0; i < m.length; i++) {
+  for (i = 0; i < m.length; i++) {
     var mval = m[i].options[m[i].selectedIndex].value;
     if(mval!=' none'){
       mod.append(mval);
     }
   }
   mod.winDock();
-}
+};
 mod.append = function(name, before){
   url = 'https://api.github.com/repos/lfpnk0/ccmsr/contents/'+name+'/index.htm?ref=gh-pages';
   var xhr = new XMLHttpRequest();
   xhr.open('GET', url);
   xhr.setRequestHeader("Accept", "application/vnd.github.3.raw");
   xhr.send();
-  xhr.onload = function(e){
+  xhr.onload = function(){
     var e = document.createElement('div');
     e.className = 'module';
     e.innerHTML = xhr.responseText;
@@ -75,8 +75,8 @@ mod.append = function(name, before){
     else{
       document.body.insertBefore(e, before);
     }
-  }
-}
+  };
+};
 mod.winResize = function(){
   var sx = screen.availWidth;
   var sy = screen.availHeight;
@@ -86,7 +86,7 @@ mod.winResize = function(){
   var y = (sy-dy)/2;
   window.moveTo(x, y);
   window.resizeTo(dx+10,dy+15);
-}
+};
 mod.winDock = function(dir){
   if(typeof(dir)==='undefined'){dir='left';}
   switch(dir){
@@ -107,21 +107,22 @@ mod.winDock = function(dir){
       window.resizeTo(screen.availWidth,250);
       break;
   }
-}
+};
 mod.loadResource = function(filePath){
+  var e;
   var ext = filePath.substr(filePath.lastIndexOf('.'),4);
-  alert(ext);
   switch(ext){
-    case 'js':
-      var e=document.createElement('script');
+    case '.js':
+      e=document.createElement('script');
       e.setAttribute('type','text/javascript');
       e.setAttribute('src', filePath);
       break;
-    case 'css':
-      var e=document.createElement('link');
+    case '.css':
+      e=document.createElement('link');
       e.setAttribute('rel', 'stylesheet');
       e.setAttribute('type', 'text/css');
       e.setAttribute('href', filePath);
   }
+};
 mod.loadResource('http://lfpnk0.github.io/ccmsr/style.css');
 mod.list();
