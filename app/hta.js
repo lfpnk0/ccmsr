f28fe844674d2b150d81660b94157517ddaf6b09
lfpnk0 <4between5@gmail.com>
2016-09-26 12:33:20 -0600

    Update hta.js

--- app/hta.js
+++ app/hta.js
@@ -1,8 +1,3 @@
-function init(){
-  window.moveTo(screen.availWidth-227, 0);
-  window.resizeTo(227, screen.availHeight);
-  var h = screen.availHeight-40;
-  document.getElementsByTagName('iframe')[0].height = h;
-  document.getElementsByTagName('iframe')[0].contentWindow.document.body = h;
-}
-init();
+window.moveTo(screen.availWidth-227, 0);
+window.resizeTo(227, screen.availHeight);
+document.getElementsByTagName('iframe')[0].height = screen.availHeight-40;
