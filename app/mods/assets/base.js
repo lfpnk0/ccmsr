0b7e06cacb9eeea5d9a82cd8b496aaf015336803
lfpnk0 <4between5@gmail.com>
2016-09-26 14:02:40 -0600

    Update base.js

--- app/mods/assets/base.js
+++ app/mods/assets/base.js
@@ -1,21 +1,27 @@
-  function collapse(){
+  function collapse(mod){
    var c = document.getElementById('content').style.display;
    var s = document.getElementById('settings').style.display;
    if(s == 'block' || c == 'block'){
     document.getElementById('content').style.display = 'none';
     document.getElementById('settings').style.display = 'none';
+    window.parent.document.getElementById(mod).height = '31 px';
    }
    else{
     document.getElementById('content').style.display = 'block';
+    window.parent.document.getElementById(mod).height = [mod].height;
    }
   }
-  function settings(){
+  function settings(mod){
    var s = document.getElementById('settings').style.display;
+   var c = document.getElementById('content').style.display;
+   if(s == 'none' && c == 'none'){
+     collapse(mod);
+   }
    if(s == 'block'){
     document.getElementById('content').style.display = 'block';
     document.getElementById('settings').style.display = 'none';
    }
-   else{
+   if(c == 'block'){
     document.getElementById('content').style.display = 'none';
     document.getElementById('settings').style.display = 'block';
    }
