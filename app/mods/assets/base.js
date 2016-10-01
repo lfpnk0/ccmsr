2fa622d2448db5c694615dc4a233a53287860218
lfpnk0 <4between5@gmail.com>
2016-10-01 10:17:05 -0600

    Update base.js

--- app/mods/assets/base.js
+++ app/mods/assets/base.js
@@ -30,6 +30,7 @@
     window.parent.document.getElementById('qk').height = h+'px';
     var arr = document.getElementsByClassName('scrollable');
     for (i = 0; i < arr.length; i++) { 
-      arr[i].height = (h-100)+'px';
+      arr[i].style.height = (h-100)+'px';
+      arr[i].style.backgroundColor = 'red';
     }
   }
