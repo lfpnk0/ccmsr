bf9f0e6018577c74ab96bc567ef2838ff7c5858d
lfpnk0 <4between5@gmail.com>
2016-10-02 13:52:24 -0600

    Update hta.js

--- app/hta.js
+++ app/hta.js
@@ -3,12 +3,11 @@ window.resizeTo(225, screen.availHeight);
 document.getElementsByTagName('iframe')[0].height = screen.availHeight-40;
 
 function getFileData(user, repo, branch, path, fileObj, callback){
- /*
  var url = 'https://api.github.com/repos/'+user+'/'+repo+'/contents/'+path+'?ref='+branch;
  var xmlhttp = new XMLHttpRequest();
  xmlhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
-   var obj = JSON.parse(this.responseText);
+/*   var obj = JSON.parse(this.responseText);
    fileObj.user = user;
    fileObj.repo = repo;
    fileObj.branch = branch;
@@ -16,12 +15,11 @@ function getFileData(user, repo, branch, path, fileObj, callback){
    fileObj.sha = obj.sha;
    fileObj.download_url = obj.download_url;
    if(typeof callback === 'function'){
-    callback(fileObj);
+    callback(fileObj); */
+   alert(this.responseText);
    }
   }
  };
  xmlhttp.open('GET', url, true);
  xmlhttp.send();
- */
- alert('yes');
 }
