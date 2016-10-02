e039a7c5119456f0d6bff71d7508bd2dea00dc7e
lfpnk0 <4between5@gmail.com>
2016-10-02 09:17:25 -0600

    Update base.js

--- app/mods/assets/base.js
+++ app/mods/assets/base.js
@@ -45,7 +45,7 @@ function getSettings(){
  var xmlhttp = new XMLHttpRequest();
  xmlhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
-   return JSON.parse(this.responseText);
+   return this.responseText;
   }
  };
  xmlhttp.open('GET', url, true);
