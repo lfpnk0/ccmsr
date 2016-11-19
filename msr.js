9085dc049ce70c52fc4a1cac583599c0c69a9530
lfpnk0 <4between5@gmail.com>
2016-11-19 14:38:24 -0700

    Create msr.js

---
+++ msr.js
@@ -0,0 +1,10 @@
+var el = document.createElement('link');
+el.setAttribute('rel', 'stylesheet');
+el.setAttribute('type', 'text/css');
+el.setAttribute('href', 'http://lfpnk0.github.io/ccmsr/msr.css');
+document.getElementsByTagName('head')[0].appendChild(el);
+var el = document.createElement('iframe');
+el.src = 'http://lfpnk0.github.io/ccmsr/app';
+el.setAttribute('scrolling', 'no');
+el.setAttribute('application', 'yes');
+document.getElementsByTagName("body")[0].appendChild(element);
