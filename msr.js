f35e86cb32736f39dc508d9d49a73c56e898c9e4
lfpnk0 <4between5@gmail.com>
2016-11-19 15:12:26 -0700

    Update msr.js

--- msr.js
+++ msr.js
@@ -1,13 +1,14 @@
 function init(){
+  alert('hi');
   var el = document.createElement('link');
   el.setAttribute('rel', 'stylesheet');
   el.setAttribute('type', 'text/css');
   el.setAttribute('href', 'http://lfpnk0.github.io/ccmsr/msr.css');
   document.getElementsByTagName('head')[0].appendChild(el);
-  var el = document.createElement('iframe');
+  el = document.createElement('iframe');
   el.src = 'http://lfpnk0.github.io/ccmsr/app';
   el.setAttribute('scrolling', 'no');
   el.setAttribute('application', 'yes');
-  document.getElementsByTagName("body")[0].appendChild(element);
+  document.getElementsByTagName('body')[0].appendChild(element);
 }
 init();
