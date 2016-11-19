d36a9245fc0766ecedc52a6b4ca444bbc301a998
lfpnk0 <4between5@gmail.com>
2016-11-19 15:09:31 -0700

    Update msr.js

--- msr.js
+++ msr.js
@@ -1,10 +1,13 @@
-var el = document.createElement('link');
-el.setAttribute('rel', 'stylesheet');
-el.setAttribute('type', 'text/css');
-el.setAttribute('href', 'http://lfpnk0.github.io/ccmsr/msr.css');
-document.getElementsByTagName('head')[0].appendChild(el);
-var el = document.createElement('iframe');
-el.src = 'http://lfpnk0.github.io/ccmsr/app';
-el.setAttribute('scrolling', 'no');
-el.setAttribute('application', 'yes');
-document.getElementsByTagName("body")[0].appendChild(element);
+function init(){
+  var el = document.createElement('link');
+  el.setAttribute('rel', 'stylesheet');
+  el.setAttribute('type', 'text/css');
+  el.setAttribute('href', 'http://lfpnk0.github.io/ccmsr/msr.css');
+  document.getElementsByTagName('head')[0].appendChild(el);
+  var el = document.createElement('iframe');
+  el.src = 'http://lfpnk0.github.io/ccmsr/app';
+  el.setAttribute('scrolling', 'no');
+  el.setAttribute('application', 'yes');
+  document.getElementsByTagName("body")[0].appendChild(element);
+}
+init();
