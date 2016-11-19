6ba7874a4a3ad2c834a940f36a8f4396829ba510
lfpnk0 <4between5@gmail.com>
2016-11-19 15:18:07 -0700

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,3 +1,3 @@
 parent.window.moveTo(screen.availWidth-225, 0);
 parent.window.resizeTo(225, screen.availHeight);
-parent.document.getElementsByTagName('iframe')[0].height = screen.availHeight-20;
+parent.document.getElementsByTagName('iframe')[0].height = screen.availHeight;
