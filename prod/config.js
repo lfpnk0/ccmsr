a1c0ced0b72669b18615671b3091adeaa94a9163
lfpnk0 <4between5@gmail.com>
2017-06-26 18:09:35 -0600

    Create config.js

---
+++ prod/config.js
@@ -0,0 +1,12 @@
+{
+	"ui": {
+		"url": "http://www.google.com",
+		"path": "c:/program files/internet explorer/iexplore.exe",
+		"flags": "-k -extoff"
+	},
+	"server": {
+		"host":"",
+		"port":"8888",
+		"queueSize":5
+	}
+}
